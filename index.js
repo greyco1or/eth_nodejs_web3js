4d66afcef901c207023c68879ebdfec062f7ba88
Grey Kim <starbux630@naver.com>
2023-05-15 14:31:33 +0900

    sec commit

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ function Subscribe() {
         if(err) {
             console.error(err);
         } else {
-            console.log("########################## Transaction Info Start ########################")
+            console.log("########################## Transaction Log Info Start ########################")
             let txId = result.transactionHash;
-            //console.log(`Block Number: ${result.blockNumber}`);
+            console.log(`Block Number: ${result.blockNumber}`);
             console.log(`Transaction log Index: ${result.logIndex}`);
             console.log(`Transaction Hash: ${txId}`);
             console.log(`Transaction Index: ${result.transactionIndex}`);
-            console.log("########################## Transaction Info Finish ########################")
             getReciptData(txId);
+            console.log("########################## Transaction Log Info Finish ########################");
         }
     })
     .on("connected", function(subscriptionId) {
@@ -30,33 +30,27 @@ function Subscribe() {
 }
 
 async function getReciptData(txId) {
-    web3.eth.getTransactionReceipt(txId).then((txReceipt) => {
-        let logs = txReceipt.logs;
-        for(let log of logs) {
-            var topics = log.topics;
-            if (topics[0]=="0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef") {
-                console.log("################# TRANSACTION RECEIPT START ####################");
-                console.log(`Transaction Hash: ${txId}`);
-                let fromAddr = web3.utils.toChecksumAddress('0x' + topics[1].substring(26));
-                let toAddr = web3.utils.toChecksumAddress('0x' + topics[2].substring(26));
-                let tokenValue = web3.utils.hexToNumberString(log.data);
-                let gasFee = txReceipt.gasUsed * txReceipt.effectiveGasPrice;
-                let contractAddress = log.address;
-                const contract = new web3.eth.Contract(erc20Abi, contractAddress);
-                //contract.methods.name().call().then((_tokenName) => console.log(_tokenName));
-                //contract.methods.decimals().call().then((_tokenDecimal) => tokenDecimal = _tokenDecimal);
-                getSmartContractData(contract).then((tokenObj) => {
-                    console.log(`Transaction From Address: ${fromAddr}`);
-                    console.log(`Transaction To Address: ${toAddr}`);
-                    console.log(`Transaction SmartContract Address: ${contractAddress}`);
-                    console.log(`Transaction Token Name: ${tokenObj.tokenName}`);
-                    console.log(`Transaction Token Value: ${tokenValue / (10 ** tokenObj.tokenDecimal)}`);
-                    console.log(`Transaction Gas Fee: ${web3.utils.fromWei(gasFee.toString(), 'ether')} ETH`);
-                    console.log("################### TRANSACTION RECEIPT FINISH #################");
-                })
-            }
+    const txReceipt = await web3.eth.getTransactionReceipt(txId);
+    const { logs } = txReceipt;
+    logs.map(async ({ topics, address, data }) => {
+        if (topics[0]=="0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef") {
+            const fromAddr = web3.utils.toChecksumAddress("0x" + topics[1].substring(26));
+            const toAddr = web3.utils.toChecksumAddress("0x" + topics[2].substring(26));
+            const tokenValue = web3.utils.hexToNumberString(data);
+            const gasFee = txReceipt.gasUsed * txReceipt.effectiveGasPrice;
+            const contract = new web3.eth.Contract(erc20Abi, address);
+            const tokenObj = await getSmartContractData(contract);
+            console.log("################# TRANSACTION RECEIPT START ####################");
+            console.log(`Transaction Hash: ${txId}`);
+            console.log(`Transaction From Address: ${fromAddr}`);
+            console.log(`Transaction To Address: ${toAddr}`);
+            console.log(`Transaction SmartContract Address: ${address}`);
+            console.log(`Transaction Token Name: ${tokenObj.tokenName}`);
+            console.log(`Transaction Token Value: ${tokenValue / 10 ** tokenObj.tokenDecimal}`);
+            console.log(`Transaction Gas Fee: ${web3.utils.fromWei(gasFee.toString(), "ether")} ETH`);
+            console.log("################### TRANSACTION RECEIPT FINISH #################");
         }
-    })
+    });
 }
 
 async function getSmartContractData(contract) {
@@ -66,10 +60,10 @@ async function getSmartContractData(contract) {
             this.tokenDecimal = tokenDecimal;
         }
     }
-    let tokenName = await contract.methods.name().call();
-    let tokenDecimal = await contract.methods.decimals().call();
+    const tokenName = await contract.methods.name().call();
+    const tokenDecimal = await contract.methods.decimals().call();
 
-    let tokenObj = new tokenData(tokenName, tokenDecimal);
+    const tokenObj = new tokenData(tokenName, tokenDecimal);
 
     return tokenObj;
 }   
